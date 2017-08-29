17627fab7e731933f3cd6de7ef5eafb3d89b0ac3
Irvin Rosas <irvinerp@gmail.com>
2017-08-29 13:27:04 -0500

    added splash screen

--- app/src/main/java/com/tunashields/wand/SplashActivity.java
+++ app/src/main/java/com/tunashields/wand/SplashActivity.java
@@ -1,13 +1,15 @@
 package com.tunashields.wand;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
