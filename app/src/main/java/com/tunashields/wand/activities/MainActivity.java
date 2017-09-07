5401e07bda75de84c2ab36c5179dc30c2c18ff85
Irvin Rosas <irvinerp@gmail.com>
2017-09-07 12:45:48 -0500

    added run-time permissions request

--- app/src/main/java/com/tunashields/wand/activities/MainActivity.java
+++ app/src/main/java/com/tunashields/wand/activities/MainActivity.java
@@ -1,9 +1,14 @@
 package com.tunashields.wand.activities;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -18,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     WandDevicesAdapter mAdapter;
 
+    private BluetoothAdapter mBluetoothAdapter;
+    private final int MY_REQUEST_ENABLE_BT = 101;
+    private final int MY_REQUEST_ENABLE_GPS = 102;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,18 +36,42 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new WandDevicesAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
 
-        mAdapter.add(new WandDevice("car", "Mustang", "Irvin", false));
+        /*mAdapter.add(new WandDevice("car", "Mustang", "Irvin", false));
         mAdapter.add(new WandDevice("garage", "VMW", "Irvin", true));
-        mAdapter.add(new WandDevice("garage", "House Garage", "Irvin", false));
+        mAdapter.add(new WandDevice("garage", "House Garage", "Irvin", false));*/
 
         setVisibleLayout(mAdapter.getItemCount() <= 0);
 
-        checkBluetooth();
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (checkGPS()) {
+            checkBluetooth();
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case MY_REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    // TODO: 9/7/17 Looking for paired devices
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    //Bluetooth not enabled.
+                    finish();
+                }
+                break;
+            case MY_REQUEST_ENABLE_GPS:
+                LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    checkBluetooth();
+                } else {
+                    finish();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     public void setVisibleLayout(boolean value) {
@@ -47,25 +80,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkBluetooth() {
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(this);
+            AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(MainActivity.this);
             mDialogBuilder.setTitle(getString(R.string.warning_disable_bluetooth_title));
             mDialogBuilder.setMessage(getString(R.string.warning_disable_bluetooth_message));
-            mDialogBuilder.setNegativeButton(getString(R.string.label_settings), new DialogInterface.OnClickListener() {
+            mDialogBuilder.setNegativeButton(getString(R.string.label_cancel), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
-                    startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+                    finish();
                 }
             });
-            mDialogBuilder.setPositiveButton(getString(R.string.label_ok), new DialogInterface.OnClickListener() {
+            mDialogBuilder.setPositiveButton(getString(R.string.label_activate), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, MY_REQUEST_ENABLE_BT);
                 }
             });
-            mDialogBuilder.create().show();
+            mDialogBuilder.show();
         }
     }
 
@@ -75,5 +109,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void startSettingsActivity(View view) {
         startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+    }
+
+    public boolean checkGPS() {
+        LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(getString(R.string.warning_disable_gps_message));
+            builder.setPositiveButton(getString(R.string.label_enable), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent callGPSSettingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivityForResult(callGPSSettingIntent, MY_REQUEST_ENABLE_GPS);
+                    dialog.dismiss();
+                }
+            });
+            builder.setNegativeButton(getString(R.string.label_cancel), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                    finish();
+                }
+            });
+            builder.setCancelable(false);
+            builder.create().show();
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- app/src/main/java/com/tunashields/wand/activities/SplashActivity.java
+++ app/src/main/java/com/tunashields/wand/activities/SplashActivity.java
@@ -1,28 +1,86 @@
 package com.tunashields.wand.activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
+import com.tunashields.wand.R;
 import com.tunashields.wand.data.AppPreferences;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(SplashActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(SplashActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(getString(R.string.label_wand_need_access_to_some_permissons));
+                builder.setNegativeButton(getString(R.string.label_cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        finish();
+                    }
+                });
+                builder.setPositiveButton(getString(R.string.label_enable), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+                    }
+                });
+                builder.create().show();
+            } else {
+                ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+            }
+        } else {
+            launchActivity();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    launchActivity();
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+                    builder.setMessage(getString(R.string.warning_can_not_scan_without_location_permission));
+                    builder.setPositiveButton(getString(R.string.label_ok), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            finish();
+                        }
+                    });
+                    builder.create().show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void launchActivity() {
         AppPreferences mPreferences = new AppPreferences(this);
-
         Intent intent;
-
         if (mPreferences.isFirstTimeOpen()) {
             mPreferences.setFirstTimeOpen(false);
             intent = new Intent(this, OnboardingActivity.class);
         } else {
             intent = new Intent(this, MainActivity.class);
         }
-
         startActivity(intent);
         finish();
     }
